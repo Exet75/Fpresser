7c83cde4de4f736d20e5734401287254d5c12168
Exet <89310199+Exet75@users.noreply.github.com>
2022-06-01 18:19:16 +0300

    Update awake.sh

--- awake.sh
+++ awake.sh
@@ -1,9 +1,20 @@
-echo '---------------------------'
-echo '---------------------------'
+echo ''
+echo ''
 echo '_______HELLO,USER!_________'
 echo 'FPRESSER IS' 
-echo '       FREE TOOL FOR OSINT!' 
+echo '       FREE TOOL FOR 11111!' 
 echo '---------------------------'
-echo '____|good luck to you|_____'
-echo '---------------------------'
-echo '---------------------------'
+echo '   Please, chose '
+echo '         your platform:   '
+echo '------------------------------'
+echo -n '[1] -LINUX   [2] -TERMUX :  '
+read VAR 
+if [[ $VAR = 1 ]]
+then
+  bash packages_for_linux.sh  
+elif [[ $VAR = 2 ]]
+then
+  bash packages_for_termux.sh
+fi
+echo ''
+echo ''   
