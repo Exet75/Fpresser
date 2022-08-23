bed825f14492ac72b309b55fb388e551e57d5993
Exet <89310199+Exet75@users.noreply.github.com>
2022-08-23 21:47:56 +0300

    Delete py.py

--- py.py
+++
@@ -1,8 +0,0 @@
-print("----------------------------------------------------------")
-ttt = input("Do you really want to continue? If yes press Enter: ")
-
-ttt = 1 
-
-if ttt > 0:
-     print("Processed...")
-     
